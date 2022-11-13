5f25c3cac6d7287a748cf754e3442a8c9061cb77
Artem <rahmat97@mail.ru>
2022-11-13 14:21:14 +0100

    added new user render

--- games-front/src/js/scripts.js
+++ games-front/src/js/scripts.js
@@ -1,1 +1,15 @@
+async function loadPlayers(){
+    const data = await fetch("http://localhost:1337/api/players")
+    const players = await data.json()
+    console.log(players)
+    players.data.forEach(printPlayer);
+}
+
+window.addEventListener("load",loadPlayers)
+function printPlayer(player){
+    const element = document.querySelector("h2")
+    element.innerText = element.innerText + "\n" +player.attributes.Name
+}
+
+
 
