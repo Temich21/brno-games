d5429d7fa0a69b31eb0114948aba3b154d03fb6b
Mishalov Evgenii <emishalov@securityscorecard.io>
2023-02-07 20:05:33 +0100

    react way load news

--- games-front-ssr/src/components/Cards/newsCards.js
+++ games-front-ssr/src/components/Cards/newsCards.js
@@ -1,29 +1,59 @@
-import styles from "./Cards.module.css"
+import { useEffect, useState } from "react";
+import styles from "./Cards.module.css";
 
-const backendUrl = "http://localhost:1337"
+const backendUrl = "http://localhost:1337";
 
-const LoadNews = async () => {
+const LoadNews = () => {
+  const [news, setNews] = useState({ data: [] });
+
+  const loadNews = async () => {
     const data = await fetch(
-        `${backendUrl}/api/news?populate=*&sort[0]=publishedAt:desc`
-    )
-    const news = await data.json()
-    const newsElements = news.data.map((data) => < CreateNewsCard key={data.id} data={data.attributes} />)
-    return (
-        <div>{newsElements}</div>
-    )
-}
+      `${backendUrl}/api/news?populate=*&sort[0]=publishedAt:desc`
+    );
+    const news = await data.json();
+    setNews(news);
+  };
 
-const CreateNewsCard = (props) => {
-    const data = props.data
-    return (
-        <article className={data.Image.data != null ? styles.contentWithImage : styles.contentWithoutImage}>
-            <img src={data.Image.data != null ? backendUrl + data.Image.data.attributes.url : ''} placeholder="blur" className={styles.picture}></img>
-            <div className={data.Image.data != null ? styles.contentWithImageContent : styles.contentWithouImageContent}>
-                <h3 className={styles.banner}>{data.Title}</h3>
-                <h4 className={styles.text}>{data.Text}</h4>
-            </div>
-        </article >
-    )
-}
+  useEffect(() => {
+    loadNews();
+  }, []);
 
-export default LoadNews
+  const newsElements = news.data.map((data) => (
+    <NewsCard key={data.id} data={data.attributes} />
+  ));
+  return <div>{newsElements}</div>;
+};
+
+const NewsCard = ({ data }) => {
+  return (
+    <article
+      className={
+        data.Image.data != null
+          ? styles.contentWithImage
+          : styles.contentWithoutImage
+      }
+    >
+      <img
+        src={
+          data.Image.data != null
+            ? backendUrl + data.Image.data.attributes.url
+            : ""
+        }
+        placeholder="blur"
+        className={styles.picture}
+      ></img>
+      <div
+        className={
+          data.Image.data != null
+            ? styles.contentWithImageContent
+            : styles.contentWithouImageContent
+        }
+      >
+        <h3 className={styles.banner}>{data.Title}</h3>
+        <h4 className={styles.text}>{data.Text}</h4>
+      </div>
+    </article>
+  );
+};
+
+export default LoadNews;
